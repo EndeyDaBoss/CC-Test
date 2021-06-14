286d6b5f0feb1dbbbbcac2a4487946ef712c0ced
EndeyDaBoss <39210430+EndeyDaBoss@users.noreply.github.com>
2021-06-13 23:00:05 -0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,21 +1,20 @@
-detector = peripheral.wrap("left")
-monitor = peripheral.wrap("top")
-detectRange = 750
+detector = peripheral.find("playerDetector")
+monitor = peripheral.find("monitor")
+detectRange = 2000
 
 monitor.setBackgroundColor(colors.black)
 monitor.setTextColor(colors.white)
 monitor.setCursorBlink(false)
 
-while true do
-    local foundPlayers = detector.getPlayersInRange(detectRange)
+while detector.isPlayersInRange(detectRange) do
+    local players = detector.getPlayersInRange(detectRange)
     local h = 1
     monitor.clear()
-    for k,v in pairs(foundPlayers) do
+    for k,v in pairs(players) do
         local pos = detector.getPlayerPos(v)
-        local player = v
         monitor.setCursorPos(1, h)
         h = h + 1
-        monitor.write(player .." - x" .. pos.x .. " y" .. pos.y .. " z" .. pos.z)
+        monitor.write(v .." - x" .. pos.x .. " y" .. pos.y .. " z" .. pos.z)
     end
     sleep(1)
 end
