8a6d72912ed31edc0c0d0ad9beef8b9a5b0b0053
EndeyDaBoss <39210430+EndeyDaBoss@users.noreply.github.com>
2021-06-13 17:13:10 -0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,14 +5,13 @@ detectRange = 750
 
 while true do
     local foundPlayers = detector.getPlayersInRange(detectRange)
-    local w, _ = monitor.getSize()
     local h = 1
+    monitor.clear()
     for k,v in pairs(foundPlayers) do
         local pos = detector.getPlayerPos(v)
-        monitor.setCursorPos(w/2, h)
-        h = h - 1
+        monitor.setCursorPos(1, h)
+        h = h + 1
         monitor.write(v .." - x" .. pos.x .. " y" .. pos.y .. " z" .. pos.z)
     end
     sleep(1)
-    monitor.clear()
 end
